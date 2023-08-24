af48f4b0cd9a56fb971c2505ce83351f242bc9db
varlikyucel <varlikyucel@gmail.com>
2023-08-24 18:13:55 +0300

    AR User Interface Main Menus

---
+++ Assets/UI/Scripts/ImageController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using System.Collections;
+
+public class ImageController : MonoBehaviour
+{
+    public GameObject[] infoPanels; // Reference to the information panels.
+    public float fadeSpeed = 0.5f; // Speed of the fade-in and fade-out.
+
+    private int currentActivePanel = -1; // Keep track of the currently active panel.
+    private bool isFading = false;
+
+    public void ShowInfo(int panelIndex)
+    {
+        if (currentActivePanel == panelIndex || isFading) return;
+
+        // Start fading out the currently active panel.
+        if (currentActivePanel >= 0)
+        {
+            StartCoroutine(FadeOut(infoPanels[currentActivePanel].GetComponent<CanvasGroup>()));
+        }
+
+        // Start fading in the selected panel.
+        StartCoroutine(FadeIn(infoPanels[panelIndex].GetComponent<CanvasGroup>()));
+
+        currentActivePanel = panelIndex;
+    }
+
+    private IEnumerator FadeIn(CanvasGroup canvasGroup)
+    {
+        isFading = true;
+        float progress = 0f;
+
+        // Set the starting state.
+        canvasGroup.alpha = 0f;
+        canvasGroup.gameObject.SetActive(true);
+
+        // Fade in over time.
+        while (progress < 1f)
+        {
+            canvasGroup.alpha = Mathf.Lerp(0f, 1f, progress);
+            progress += Time.deltaTime * fadeSpeed;
+            yield return null;
+        }
+
+        canvasGroup.alpha = 1f;
+        isFading = false;
+    }
+
+    private IEnumerator FadeOut(CanvasGroup canvasGroup)
+    {
+        isFading = true;
+        float progress = 0f;
+
+        // Fade out over time.
+        while (progress < 1f)
+        {
+            canvasGroup.alpha = Mathf.Lerp(1f, 0f, progress);
+            progress += Time.deltaTime * fadeSpeed;
+            yield return null;
+        }
+
+        canvasGroup.alpha = 0f;
+        canvasGroup.gameObject.SetActive(false);
+        isFading = false;
+    }
+
+    public void CloseAllPanels()
+    {
+        if (currentActivePanel >= 0)
+        {
+            StartCoroutine(FadeOut(infoPanels[currentActivePanel].GetComponent<CanvasGroup>()));
+            currentActivePanel = -1;
+        }
+    }
+}
---
+++ Assets/UI/Scripts/ImageSwitcher.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageSwitcher : MonoBehaviour
+{
+    public Sprite[] imageVariations; // Drag your 6 images here in the inspector
+    [Range(0, 5)] // Assuming you have 6 images
+    public int currentImageIndex = 0; // Change this in the inspector to switch image
+
+    private Image _imageComponent;
+
+    private void Start()
+    {
+        _imageComponent = GetComponent<Image>();
+        UpdateImage();
+    }
+
+    private void OnValidate()
+    {
+        UpdateImage();
+    }
+
+    private void UpdateImage()
+    {
+        // Initialize _imageComponent if it's null
+        if (_imageComponent == null)
+        {
+            _imageComponent = GetComponent<Image>();
+
+            // If it's still null after attempting to get the component, log an error and exit the method.
+            if (_imageComponent == null)
+            {
+                Debug.LogError("No Image component found on this GameObject!");
+                return;
+            }
+        }
+
+        if (imageVariations != null && imageVariations.Length > 0 && currentImageIndex >= 0 && currentImageIndex < imageVariations.Length)
+        {
+            _imageComponent.sprite = imageVariations[currentImageIndex];
+        }
+    }
+
+
+}
