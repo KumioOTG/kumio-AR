26629a8668c8715909c061710838c6fad325eb78
bgmm <begummoralioglu@gmail.com>
2023-08-25 21:17:12 +0300

    tutorial KGO-AR

--- Assets/ParentObjectManagerAR1.cs
+++ Assets/ParentObjectManagerAR1.cs
@@ -10,10 +10,22 @@ public class ParentObjectManagerAR1 : MonoBehaviour
     [SerializeField]
     private GameObject nextParentObject; // Set this in the inspector
 
+    [SerializeField]
+    private AudioClip collectionSound; // The sound clip to play. Set this in the inspector.
+
+    private AudioSource audioSource; // This will play the sound
+
     private int collectibleCoinCount = 0;
 
     private void Start()
     {
+        // Initialize the audio source component
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
         // Register for notifications when a coin is collected
         InventoryManager.OnCollect += HandleCoinCollected;
     }
@@ -32,6 +44,12 @@ public class ParentObjectManagerAR1 : MonoBehaviour
             collectibleCoinCount++;
             if (collectibleCoinCount % 3 == 0)
             {
+                // Play the collection sound
+                if (collectionSound != null && audioSource != null)
+                {
+                    audioSource.PlayOneShot(collectionSound);
+                }
+
                 // Deactivate current parent object and activate next parent object
                 if (currentParentObject != null) currentParentObject.SetActive(false);
                 if (nextParentObject != null) nextParentObject.SetActive(true);
---
+++ Assets/Scripts/ActivateNextObjectUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateNextObjectUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToActivate;  // The object you want to activate
+
+    [SerializeField]
+    private GameObject objectToDeactivate;  // The object you want to deactivate
+
+    [SerializeField]
+    private float delayTime = 1.0f; // Default delay of 1 second, but you can set this from the editor
+
+    void Start()
+    {
+        // Add a listener to the button component, which triggers when the button is clicked
+        GetComponent<Button>().onClick.AddListener(HandleClick);
+    }
+
+    void HandleClick()
+    {
+        // Use StartCoroutine to implement the delay
+        StartCoroutine(DelayedSwapActivation());
+    }
+
+    System.Collections.IEnumerator DelayedSwapActivation()
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        // Deactivate the specified object
+        if (objectToDeactivate != null)
+        {
+            objectToDeactivate.SetActive(false);
+        }
+
+        // Activate the specified object
+        if (objectToActivate != null)
+        {
+            objectToActivate.SetActive(true);
+        }
+    }
+}
