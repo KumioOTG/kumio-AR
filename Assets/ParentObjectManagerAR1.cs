79ca78cd28a0daac9f420f1210567899a0de953f
bgmm <begummoralioglu@gmail.com>
2023-08-26 14:37:18 +0300

    sound update

--- Assets/ParentObjectManagerAR1.cs
+++ Assets/ParentObjectManagerAR1.cs
@@ -10,10 +10,22 @@ public class ParentObjectManagerAR1 : MonoBehaviour
     [SerializeField]
     private GameObject nextParentObject; // Set this in the inspector
 
+    [SerializeField]
+    private AudioClip successSound; // The success sound clip to play. Set this in the inspector.
+
+    private AudioSource audioSource; // This will play the sound
+
     private int collectibleCoinCount = 0;
 
     private void Start()
     {
+        // Initialize the audio source component
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
         // Register for notifications when a coin is collected
         InventoryManager.OnCollect += HandleCoinCollected;
     }
@@ -26,12 +38,18 @@ public class ParentObjectManagerAR1 : MonoBehaviour
 
     private void HandleCoinCollected(GameObject collectedObject)
     {
-        // Check if the collected object has the "CollectibleCoin" tag
+        // Check if the collected object has the "Collectible" tag
         if (collectedObject.CompareTag("Collectible"))
         {
             collectibleCoinCount++;
             if (collectibleCoinCount % 3 == 0)
             {
+                // Play the success sound
+                if (successSound != null && audioSource != null)
+                {
+                    audioSource.PlayOneShot(successSound);
+                }
+
                 // Deactivate current parent object and activate next parent object
                 if (currentParentObject != null) currentParentObject.SetActive(false);
                 if (nextParentObject != null) nextParentObject.SetActive(true);
